d5dd09317b476a917bd6471d71c3b9cb4e533dc7
Shankar-001 <praveen01.ugec20@iiitranchi.ac.in>
2023-12-28 01:48:02 +0530

    api error and response

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 // require('dotenv').config({path: './.env'})
 import dotenv from 'dotenv';
+import app from './app.js';
 import connectDB from './db/index.js';
 
 dotenv.config({
   path: './.env',
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('DB connection failed !!!', err);
+  });
+
+
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+const asyncHandler = (requestHandler) => {
+  (req, re, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+
+
+
+
+// asyncHandler is a higher-order function
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//     try {
+//         await func(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
